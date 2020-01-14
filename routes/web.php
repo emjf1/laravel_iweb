e815c9163e2388978338e6d133c46406d1229952
jvv13 <jvv13@alu.ua.es>
2020-01-14 18:15:21 +0100

    Añadidas algunas rutas de Regimen, Reserva, Habitacion, Sala y Usuario

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/prueba', 'PruebaController@list');
+
+// Regimen
+Route::get('/regimenes', 'RegimenController@listaRegimen');
+
+// Reserva
+Route::get('/reservas', 'ReservaController@listaReserva'); //Middleware
+Route::get('/usuario/{userID}/reservas', 'ReservaController@listaReservasUsuario'); //Middleware
+
+// Sala
+Route::get('/salas', 'SalaController@listaSala'); 
+Route::get('/salas/{id}', 'SalaController@detalleSala'); 
+Route::get('/salas/{id}/imagenes', 'SalaController@listaFotoSala');
+Route::put('/salas/{id}', 'SalaController@editarSala'); //Middleware
+Route::delete('/salas/{id}', 'SalaController@eliminarSala'); //Middleware
+
+// Habitacion
+Route::get('/habitaciones', 'HabitacionController@listaHabitacion'); 
+Route::get('/habitaciones/{id}', 'HabitacionController@detalleHabitacion'); 
+Route::get('/habitaciones/{id}', 'HabitacionController@listaFotoHabitacion'); 
+Route::put('/habitaciones/{id}', 'HabitacionController@editarHabitacion'); //Middleware
+Route::delete('/habitaciones/{id}', 'HabitacionController@eliminarHabitacion'); //Middleware
+
+// Usuario
+Route::get('/login', 'UsuarioController@login'); //Middleware
+Route::get('/registro', 'UsuarioController@registro'); //Middleware
+Route::get('/usuario/{userID}', 'UsuarioController@infoUsuario'); //Middleware
+Route::put('/usuario/{userID}', 'UsuarioController@editarUsuario'); //Middleware
+Route::delete('/usuario/{userID}', 'UsuarioController@eliminarUsuario'); //Middleware
