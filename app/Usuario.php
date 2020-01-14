e1c1f36ff91c6bd665583536827ec4075dfbe1c0
Francisco García Mora <frandbc7@gmail.com>
2020-01-14 13:59:53 +0100

    Añadidos cambios de Usuario.php

--- app/Usuario.php
+++ app/Usuario.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Usuario extends Model
 {
 
-    protected $fillable = ('email', 'nombre', 'apellidos', 'telefono', 'direccion', 'contraseña', 'foto_perfil', 'DNI', 'nacionalidad');
+    protected $fillable = array('email', 'nombre', 'apellidos', 'telefono', 'direccion', 'contraseña', 'foto_perfil', 'DNI', 'nacionalidad');
 
     public function tipo_usuario(){
         return $this->belongsTo('App\Tipo_usuario');
@@ -15,5 +16,37 @@ class Usuario extends Model
 
     public function reservas(){
         return $this->hasMany('App\Reserva');
+    }
+
+    public static function create(array $data){
+        $usuario = new Usuario();
+        $usuario->email = $data['email'];
+        $usuario->nombre = $data['nombre'];
+        $usuario->apellidos = $data['apellidos'];
+        $usuario->telefono = $data['telefono'];
+        $usuario->direccion = $data['direccion'];
+        $usuario->contraseña = $data['contraseña'];
+        $usuario->foto_perfil = $data['foto_perfil'];
+        $usuario->DNI = $data['DNI'];
+        $usuario->nacionalidad = $data['nacionalidad'];
+        $usuario->save();
+        return $usuario;
+    }
+
+    public function update(array $data, Usuario $usuario){
+        $usuario->update($data);
+        return $usuario;
+    }
+
+    public function delete(array $data, Usuario $usuario){
+        if($usuario->delete($data))
+            return true;
+        else
+            return false;
+    }
+
+    public static function read(Usuario $data){
+        $usuario = Usuario::where('email', '=', $data->nombre);
+        return $usuario;
     }
 }
