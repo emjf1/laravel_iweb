e8f0909a72b54b1f29bb1213319e384ca4a2dc30
Francisco García Mora <frandbc7@gmail.com>
2020-01-15 15:37:15 +0100

    Controller Reserva OK

--- app/Http/Controllers/TemporadaController.php
+++ app/Http/Controllers/TemporadaController.php
@@ -6,5 +6,45 @@ use Illuminate\Http\Request;
 
 class TemporadaController extends Controller
 {
-    //
+    public function listaTemporada()
+    {
+        $resultado = Temporada::listarTemporada();
+        return response()->json($resultado);
+    }
+
+    public function detalleTemporada($id)
+    {
+        $data = new Temporada();
+        $data->codigo = $id;
+
+        $resultado =  Temporada::mostrarTemporada($data);
+        return response()->json($resultado);
+    }
+
+    public function eliminarTemporada($id)
+    {
+        $temporada = new Temporada();
+        $temporada->codigo = $id;
+
+        $resultado = Temporada::borrarTemporada($temporada);
+        return response()->json($resultado);
+    }
+
+    public function crearTemporada(){
+        $data = request()->all();
+
+        $resultado = Temporada::crearTemporada($data);
+        return response()->json($resultado);
+    }
+
+    public function editarTemporada($id)
+    {
+        $data = request()->all();
+
+        $temporada = new Temporada();
+        $temporada->codigo = $id;
+
+        $resultado = Temporada::actualizarTemporada($data, $temporada);
+        return response()->json($resultado);
+    }
 }
