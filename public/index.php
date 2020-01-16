528087cbcbafee1c9b6a4aecb2071a2fd2cb9e78
jvv13 <jvv13@alu.ua.es>
2020-01-16 14:40:14 +0100

    Añadido header al index

--- public/index.php
+++ public/index.php
@@ -58,3 +58,9 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+
+header('Access-Control-Allow-Origin: *'); 
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
